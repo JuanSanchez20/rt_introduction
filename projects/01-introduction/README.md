f455cff6ee0f111acc790e02dd4bc74fe1f195dd
Juan Sanchez <sanchezlemaj@gmail.com>
2025-01-03 21:31:30 -0500

    Modificando los estilos de un contenedor especifico a nivel del proyecto

--- projects/01-introduction/README.md
+++ projects/01-introduction/README.md
@@ -7,3 +7,6 @@ Un componente es una funcion que crea un elemento.
 Lo principal de esta biblioteca es la reutilizacion de componentes
 
 La base de la reutilizacion de los componentes son las propiedades.
+
+Se puede realizar cambios a nivel de proyecto con contenedores y asi evitar afectar a todos los componenentes que no requieran de la modificacion.
+
--- projects/01-introduction/src/App.jsx
+++ projects/01-introduction/src/App.jsx
@@ -3,10 +3,10 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App() {
     return(
-    <>
+    <section className='App'>
         <TwitterFollowCard username='midudev' name='Miguel Ángel Durán'/>
         <TwitterFollowCard username='pherald' name='Pablo Hernandez'/>
         <TwitterFollowCard username='elonmusk' name='Elon Musk'/>
-    </>
+    </section>
     )
 }
