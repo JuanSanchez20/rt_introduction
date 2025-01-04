f7283a6a01df3087b733f874dcd2325f1f28da46
Juan Sanchez <sanchezlemaj@gmail.com>
2025-01-03 19:26:50 -0500

    Primer Hola Mundo

--- projects/01-introduction/src/main.jsx
+++ projects/01-introduction/src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    
-  </StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  "Hola mundo"
 )
