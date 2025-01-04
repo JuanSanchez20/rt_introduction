0a24529a8a5ede15d1bb305d6ac2b1e097c99083
Juan Sanchez <sanchezlemaj@gmail.com>
2025-01-03 21:07:16 -0500

    Reutilizando el comoponente

--- projects/01-introduction/src/App.jsx
+++ projects/01-introduction/src/App.jsx
@@ -5,6 +5,8 @@ export function App() {
     return(
     <>
         <TwitterFollowCard username='midudev' name='Miguel Ángel Durán'/>
+        <TwitterFollowCard username='pherald' name='Pablo Hernandez'/>
+        <TwitterFollowCard username='elonmusk' name='Elon Musk'/>
     </>
     )
 }
