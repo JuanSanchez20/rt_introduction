798be9689c8d2559112c13d1f4e52196fd36b29a
Juan Sanchez <sanchezlemaj@gmail.com>
2025-01-04 16:07:23 -0500

    Agregando la lista de combos ganadores

--- projects/02-tres-en-raya/src/App.jsx
+++ projects/02-tres-en-raya/src/App.jsx
@@ -20,10 +20,23 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const winner_combo = [
+  [0, 1, 2],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.O)
+
+  const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
     if (board[index]) return
