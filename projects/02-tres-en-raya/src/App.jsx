89d9540063648eee8e2482c68399a5eac9e0f28a
Juan Sanchez <sanchezlemaj@gmail.com>
2025-01-06 08:56:33 -0500

    Modificando la funcion de resetGame para que reinicie todos los estados a su estado inicial para poder volve a jugar

--- projects/02-tres-en-raya/src/App.jsx
+++ projects/02-tres-en-raya/src/App.jsx
@@ -54,6 +54,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.O)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   return (
