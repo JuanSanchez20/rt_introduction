96e6c23cbd7146d51b6eee91160c38fe2b340135
Juan Sanchez <sanchezlemaj@gmail.com>
2025-01-07 09:39:14 -0500

    Agregando las dependencias necesarias y agregando un boton junto con un elemento estado para activar o desactivar el seguimiento del puntero

--- projects/03_mouse_follower/src/App.jsx
+++ projects/03_mouse_follower/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
 
+  useEffect(() => {
+    console.log('useEffect')
+  })
   return (
     <>
       <h1>3er Proyecto</h1>
+      <button>{enabled ? 'Desactivar' : 'Activar'} Seguir Puntero</button>
     </>
   )
 }
