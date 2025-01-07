4e6ab1c85ae088b4ec8f8979b73efed84a3bf915
Juan Sanchez <sanchezlemaj@gmail.com>
2025-01-07 09:47:01 -0500

    Agregando el puntero para que tenga un efecto de seguimiento

--- projects/03_mouse_follower/src/App.jsx
+++ projects/03_mouse_follower/src/App.jsx
@@ -9,12 +9,25 @@ function App() {
   }, [enabled])
 
   return (
-    <>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}/>
       <h1>3er Proyecto</h1>
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} Seguir Puntero
       </button>
-    </>
+    </main>
   )
 }
 
